d066e03129e5c4011966a7d7673717e9ad80e003
Mark-Kil <markkilcanarias@gmail.com>
2023-12-18 11:34:22 +0100

    first commit

---
+++ src/test/java/dev/mark/apimovies/ApimoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.apimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
